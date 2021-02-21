3668cfe93cbe63fb873b0db2de186b91a5e6631a
Aleksandr Okunev <frozz.way@mail.ru>
2021-02-21 13:48:01 +0500

    3

--- Practice 1/source.py
+++ Practice 1/source.py
@@ -1,14 +1,40 @@
 def main():
+    array = create_array()
+    search_for_position(array)
+
+
+# Считывание числа у пользователя
+def read_number():
+    number = input()
+    try:
+        return float(number)
+    except:
+        print("Ошибка. Введи число")
+        return read_number()
+
+
+def create_array():
     print("Количество элементов в массиве равно ?")
-    n = input()
-    if not n.isnumeric():
-        print("N должно быть число")
-        main()
-        return
+    n = read_number()
     array = []
     for i in range(int(n)):
         print(f'{i}. = ')
-        array[i].append(int(input()))
+        number = read_number()
+        array.append(number)
+    return array
+
+
+def search_for_position(array):
+    filtered_array = [abs(i) for i in range(len(array)) if i < 0]
+    if len(filtered_array) == 0:
+        return -1
+    minimum = filtered_array[0]
+    position = 0
+    for i in range(len(filtered_array)):
+        if filtered_array[i] < minimum:
+            minimum = filtered_array[i]
+            position = i
+    return position
 
 
 main()
