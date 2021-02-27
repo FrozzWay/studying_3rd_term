671337528699cbec894eeb65b0ed1ca9f6cc57cd
Aleksandr Okunev <frozz.way@mail.ru>
2021-02-27 16:22:42 +0500

    2.1

--- Practice 2/source.py
+++ Practice 2/source.py
@@ -1,23 +1,35 @@
 class Record:
     def __init__(self, data):
+        self.id = data.get("id")
         self.surname = data.get("surname", "")
         self.reg_number = data.get("reg_number", "")
         self._from = data.get("from", "")
         self.up_to = data.get("up_to", "")
         self.rate = data.get("rate", "")
+        self.copy_of_data = data
         self.next = None
+
+    def __getitem__(self, key):
+        return getattr(self, key)
 
 
 class Collection:
     def __init__(self):
         self.first = None
+        self.sorted_first = None
 
     def fill(self):
+        id = 0
         while True:
             stop_input = False
 
-            data = dict.fromkeys(("surname", "reg_number", "from", "up_to", "rate"))
-            for key in data.keys():
+            data = dict()
+            keys = ("surname", "reg_number", "from", "up_to", "rate")
+
+            data['id'] = id
+            id += 1
+
+            for key in keys:
                 print(f'{key}: ')
                 value = input()
                 if value == "":
@@ -26,9 +38,10 @@ class Collection:
                 data[key] = value
 
             tmp = Record(data)
+
             if self.first is None:
                 self.first = tmp
-            else:
+            elif data.get('surname'):
                 self._put(tmp, self.first)
 
             if stop_input:
@@ -43,10 +56,22 @@ class Collection:
     def output(self):
         current = self.first
         while current is not None:
-            print(current.name)
+            for key, val in current.copy_of_data.items():
+                print(f'{key}: {val}')
             current = current.next
+
+    def sort(self, key):
+        current_unsorted = self.first
+        current_sorted = Record(self.first.copy_of_data)
+
+        # Берем каждый элемент по порядку из неотсортерованного списка (element)
+        while current_unsorted is not None:
+            # element <= first el.
+            if current_unsorted[key] <= current_sorted[key]:
+                pass
+            current_unsorted = current_unsorted.next
 
 
 collection = Collection()
 collection.fill()
-#collection.output()
+collection.output()
--- Practice 2/test.py
+++ Practice 2/test.py
@@ -1,1 +1,9 @@
-print("" > "afa")
+class A:
+    a = 'a'
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+aa = A()
+
+print(aa['a'])
